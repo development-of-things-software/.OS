750365ec27c7c9209ab56ec26de272d71e21277c
Ocawesome101 <ocawesome101@gmail.com>
2022-05-03 11:04:38 -0400

    maybe make installer a lot faster, and maube also a lot more broken

--- utils/install.lua
+++ utils/install.lua
@@ -15,14 +15,16 @@ local base = "https://raw.githubusercontent.com/development-of-things-software/.
 -- get file list
 local handle = assert(http.get(base .. "files.txt"))
 
+local total, downloaded = 0, 0
+
+term.clear()
+term.setCursorPos(1,1)
 local function download(file)
   -- strip /rom
   local ofile = file
   file = file:gsub("/rom", "")
-  print(ofile, file)
   local dir = fs.getDir(file)
   dir = fs.combine(installdir, dir)
-  print(dir)
   fs.makeDir(dir)
   local dl = assert(http.get(base .. ofile, nil, true))
   local data = dl.readAll()
@@ -30,15 +32,26 @@ local function download(file)
   local whand = assert(io.open(fs.combine(installdir, file), "wb"))
   whand:write(data)
   whand:close()
+  downloaded = downloaded + 1
+  term.setCursorPos(1, 1)
+  term.write("Downloading .OS (%d/%d)", downloaded, total)
 end
 
+local files = {}
 for line in handle.readLine do
-  download(line)
+  table.insert(files, function()
+    download(line)
+  end)
+  total = total + 1
 end
 
 handle.close()
+
+parallel.waitForAll(table.unpack(files))
 
 download("unbios.lua")
 fs.move(fs.combine(installdir, "unbios.lua"), fs.combine(installdir, "startup.lua"))
 
 fs.makeDir(fs.combine(installdir, "/users/admin"))
+
+print("\nInstall finished.")
