e7d2b4f6f42c5339080dd76d900dba01f96d3742
Ocawesome101 <ocawesome101@gmail.com>
2022-05-03 11:36:50 -0400

    one more little tiny bug

--- utils/genfilestxt.sh
+++ utils/genfilestxt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f | grep -v LICENSE | grep -v README | grep -v unbios | grep -v install | grep -v .git | grep -v files.txt | grep -v utils | grep -v dotos.mtar.lua > files.txt
+find . -type f | grep -v LICENSE | grep -v README | grep -v unbios | grep -v install | grep -v .git | grep -v files.txt | grep -v /utils | grep -v dotos.mtar.lua > files.txt
